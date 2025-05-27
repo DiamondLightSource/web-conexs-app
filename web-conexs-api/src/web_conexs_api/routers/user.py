d1e763c97e9800f147d68c044ae57d9c94b91e7a
Jacob Filik <jacob.filik@diamond.ac.uk>
2025-05-27 08:16:22 +0000

    Add initial config for user auth

---
+++ web-conexs-api/src/web_conexs_api/routers/user.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+
+from ..auth import get_current_user
+from ..crud import get_user
+from ..database import get_session
+
+router = APIRouter()
+
+
+@router.get("/api/user")
+async def check(
+    session: Session = Depends(get_session), user_id: str = Depends(get_current_user)
+):
+    return get_user(session, user_id)
