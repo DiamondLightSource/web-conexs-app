fdf727430eb19773c4677309831065c99de20020
Jacob Filik <jacob.filik@diamond.ac.uk>
2026-06-17 08:12:34 +0000

    Update version of h5web

--- web-conexs-client/src/components/RequireAuth.test.tsx
+++ web-conexs-client/src/components/RequireAuth.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import Header from "./Header";
 import { ThemeProvider, Typography } from "@mui/material";
 import { DiamondTheme } from "@diamondlightsource/sci-react-ui";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
